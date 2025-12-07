9c9c669bc36f4756061f82f07536ddb25c2ce66a
Adnan Nuredin <152042446+Adnan7389@users.noreply.github.com>
2025-12-07 23:25:39 +0300

    feat: add auth controller for better auth routes

---
+++ backend/src/auth/auth.controller.ts
@@ -0,0 +1,11 @@
+import { All, Controller, Req, Res } from '@nestjs/common';
+import { auth } from './auth.service.js';
+import { toNodeHandler } from 'better-auth/node';
+
+@Controller('auth')
+export class AuthController {
+    @All('*')
+    async handleAuth(@Req() req, @Res() res) {
+        return toNodeHandler(auth)(req, res);
+    }
+}
