3918f10c77c11ea912589b039f028d71e22af1f1
Adnan Nuredin <152042446+Adnan7389@users.noreply.github.com>
2025-12-07 23:25:55 +0300

    chore: import auth module in application root

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller.js';
+import { AuthModule } from './auth/auth.module.js';
 
 @Module({
     imports: [
@@ -13,6 +14,7 @@ import { AppController } from './app.controller.js';
             ? [MongooseModule.forRoot(process.env.MONGO_URI)]
             : []
         ),
+        AuthModule,
     ],
     controllers: [AppController],
 })
