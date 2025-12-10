2ca94b9474453bb4cf69ad8de8f606ce0117ef99
Adnan Nuredin <152042446+Adnan7389@users.noreply.github.com>
2025-12-10 17:58:16 +0300

    feat(chapters): add chapters module

---
+++ backend/src/modules/chapters/chapters.module.ts
@@ -0,0 +1,16 @@
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { ChaptersController } from "./chapters.controller.js";
+import { ChaptersService } from "./chapters.service.js";
+import { Chapter, ChapterSchema } from "./schemas/chapter.schema.js";
+import { LearningPathsModule } from "../learning-paths/learning-paths.module.js";
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Chapter.name, schema: ChapterSchema }]),
+    LearningPathsModule, // For progress update
+  ],
+  controllers: [ChaptersController],
+  providers: [ChaptersService],
+})
+export class ChaptersModule {}
