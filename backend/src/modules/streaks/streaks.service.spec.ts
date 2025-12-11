a4c379898167fda780e116c97a0af3ead0cff754
Adnan Nuredin <152042446+Adnan7389@users.noreply.github.com>
2025-12-11 17:15:00 +0300

    test: add streaks service test file
    - Add auto-generated test file for StreaksService
    - Prepare for future unit tests

---
+++ backend/src/modules/streaks/streaks.service.spec.ts
@@ -0,0 +1,23 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { StreaksService } from './streaks.service';
+import { auth } from '../../auth/auth.service';
+import { Logger } from '@nestjs/common';
+
+jest.mock('../../auth/auth.service');
+
+describe('StreaksService', () => {
+  let service: StreaksService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [StreaksService, Logger],
+    }).compile();
+
+    service = module.get<StreaksService>(StreaksService);
+  });
+
+  it('should be defined', () => {
+    console.log(auth);
+    expect(service).toBeDefined();
+  });
+});
