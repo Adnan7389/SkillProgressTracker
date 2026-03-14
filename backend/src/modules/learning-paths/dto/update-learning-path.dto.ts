5c969c9ad04d137e66cbea67d81ed6eb6bf76322
Adnan Nuredin <152042446+Adnan7389@users.noreply.github.com>
2026-03-14 10:00:00 +0300

    docs(backend): add Swagger decorators to UpdateLearningPathDto

--- backend/src/modules/learning-paths/dto/update-learning-path.dto.ts
+++ backend/src/modules/learning-paths/dto/update-learning-path.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from "@nestjs/mapped-types";
+import { PartialType } from "@nestjs/swagger";
 import { CreateLearningPathDto } from "./create-learning-path.dto.js";
 
-export class UpdateLearningPathDto extends PartialType(CreateLearningPathDto) {}
+export class UpdateLearningPathDto extends PartialType(CreateLearningPathDto) { }
